c97b4760f94563b6f957ade2dde0f6be9d790c55
silencily <silencily@126.com>
2016-02-19 17:58:06 +0800

    1.UserController

---
+++ doorche-admin/src/main/java/org/silencer/doorche/admin/sm/controller/UserController.java
@@ -0,0 +1,15 @@
+package org.silencer.doorche.admin.sm.controller;
+
+import org.silencer.doorche.admin.support.web.AbstractAdminController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author gejb
+ * @since 2016/2/19
+ */
+@Controller
+@RequestMapping("/sm/user")
+public class UserController extends AbstractAdminController {
+
+}
